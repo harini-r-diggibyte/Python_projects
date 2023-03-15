f39ac647659dfa4f052b729408d3901367d8ac85
User <harini.r@diggibyte>
2023-03-15 22:35:11 +0530

    Pyhton assignments

--- core/py_collection_namedtuple.py
+++ core/py_collection_namedtuple.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 
+#enter the number of students
 N = int(input())
 total = 0
+#enter the students : ID,MARKS,NAME,CLASS
 student = namedtuple('student', input().split())
 for i in range(N):
     field1, field2, field3, field4 = input().split()
